8b84c8b94ab05e8d122c096a5e58e1ad3ff381e1
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 22:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 let main = document.querySelector('main#main');
 main.remove();
-
+let body = document.querySelector('body');
 let newHeader = document.createElement('h1');
