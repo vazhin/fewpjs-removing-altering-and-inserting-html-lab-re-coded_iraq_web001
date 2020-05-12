cd1b6e45dd5c644fee9eb23012e4c6b4da38e998
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-05-12 22:24:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+let body = document.querySelector('body');
+body.appendChild(newHeader).id="victory";
+
+newHeader.innerHTML = 'vazhin is the champion';
